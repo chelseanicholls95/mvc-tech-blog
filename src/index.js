f608bd3fb6f0e5a19969f4fbaeb5031d00bca622
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-06-06 10:16:28 +0100

    set up sequelize connection to server

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 
-const PORT = process.env.PORT || 3000;
+const { connection } = require("./config/connection");
+
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -14,11 +16,12 @@ app.use(express.static(path.join(__dirname, "../", "public")));
 
 const init = async () => {
   try {
+    await connection.sync();
     app.listen(PORT, () =>
       console.log(`Server running on http://localhost:${PORT}`)
     );
   } catch (error) {
-    console.error("Failed to connect to DB");
+    console.error(error, "Failed to connect to DB");
   }
 };
 
