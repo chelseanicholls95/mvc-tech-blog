9aa6c0b1da4e394039d12575c05569541871db5f
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-06-06 10:25:58 +0100

    added user model

---
+++ src/models/User.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("../config/connection");
+
+const schema = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  first_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  last_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+};
+
+const options = {
+  sequelize,
+  modelName: "user",
+  timestamps: true,
+  underscored: true,
+  freezeTableName: true,
+};
+
+class User extends Model {}
+
+User.init(schema, options);
+
+module.exports = User;
