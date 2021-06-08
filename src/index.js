bb85c756e107856f96684032e7097dcc440ae25f
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-06-08 16:30:55 +0100

    added sessions

--- src/index.js
+++ src/index.js
@@ -2,15 +2,27 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const routes = require("./routes");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sequelize = require("./config/connection");
+const routes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
 app.use(cors());
+app.use(session(sessionOptions));
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../", "public")));
