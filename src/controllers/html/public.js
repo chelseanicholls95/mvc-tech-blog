73e34f12fd47019b97079295dd643bab0ae85355
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-06-12 20:34:18 +0100

    added all posts to home page

--- src/controllers/html/public.js
+++ src/controllers/html/public.js
@@ -1,6 +1,18 @@
-const renderHome = (req, res) => {
+const { User, Post } = require("../../models");
+
+const renderHome = async (req, res) => {
   try {
-    res.render("home");
+    const posts = await Post.findAll({
+      include: [
+        {
+          model: User,
+        },
+      ],
+    });
+
+    const formattedPosts = posts.map((post) => post.get({ plain: true }));
+
+    res.render("home", { posts: formattedPosts });
   } catch (error) {
     res.status(500).json({ error: "Failed to render" });
   }
