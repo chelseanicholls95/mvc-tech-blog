da94483db83b57a84d77ec4fb917db9b8921277a
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-06-12 16:01:53 +0100

    added new post button

--- src/routes/html/private.js
+++ src/routes/html/private.js
@@ -5,5 +5,6 @@ const renderDashboard = require("../../controllers/html/private");
 const router = Router();
 
 router.get("/dashboard", renderDashboard);
+router.get("/new-post");
 
 module.exports = router;
