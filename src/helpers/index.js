b53256dd54260e7f1cffc8003631efd98cc248a5
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-06-13 21:21:54 +0100

    changed date format

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,7 +2,7 @@ const moment = require("moment");
 const handlebars = require("handlebars");
 
 const formatDate = handlebars.registerHelper("formatDate", function (date) {
-  return moment(date).format("DD.MM.YYYY - hh:mm");
+  return moment(date).format("DD.MM.YYYY - HH:mm");
 });
 
 module.exports = formatDate;
