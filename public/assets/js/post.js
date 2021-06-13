bb06f400d736c66fb25ebbcdf291913c93934c62
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-06-13 21:22:24 +0100

    added create comment front end

--- public/assets/js/post.js
+++ public/assets/js/post.js
@@ -15,4 +15,32 @@ const onDelete = async (event) => {
   }
 };
 
+const onSubmit = async (event) => {
+  event.preventDefault();
+
+  const id = event.currentTarget.id;
+
+  const message = $("#comment-input").val();
+
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    redirect: "follow",
+    body: JSON.stringify({
+      message,
+    }),
+  };
+
+  const response = await fetch(`/api/posts/${id}/comment`, options);
+
+  if (response.status !== 200) {
+    console.log("Failed to create comment");
+  } else {
+    window.location.replace(`/view/${id}`);
+  }
+};
+
 $('[name="delete-btn"]').click(onDelete);
+$('[name="comment-form"]').submit(onSubmit);
